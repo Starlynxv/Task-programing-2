f5dc89be95c851c27f222a2e21a793d972800722
STARLYN CASTILLO <120070938+Starlynxv@users.noreply.github.com>
2026-02-12 15:08:50 -0400

    adding my task 1

---
+++ task1/task_1/CommunityMember.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_1
+{
+    
+    public class CommunityMember
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+
+
+    public class Employee : CommunityMember
+    {
+        public decimal Salary { get; set; }
+    }
+
+    public class Student : CommunityMember
+    {
+        public string Major { get; set; }
+    }
+
+    public class Alumnus : CommunityMember
+    {
+        public int GraduationYear { get; set; }
+    }
+
+   
+    public class Faculty : Employee
+    {
+        public string AcademicArea { get; set; }
+    }
+
+    public class Administrative : Employee
+    {
+        public string Department { get; set; }
+    }
+
+   
+    public class Administrator : Faculty
+    {
+        public string Position { get; set; }
+    }
+
+    public class Teacher : Faculty
+    {
+        public string Subject { get; set; }
+    }
+} 
---
+++ task1/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher myTeacher = new Teacher
+            {
+                Name = "Starlyn",
+                Subject = "C#"
+            };
+
+            Console.WriteLine("Everything is working! Teacher: " + myTeacher.Name);
+            Console.ReadKey();
+        }
+    }
+}   
